0d51d78f43b04af34c21ee76f90cd1551e95ad1b
Stephen J <sajames1958@gmail.com>
2017-05-24 01:14:53 +0000

    pitching stats

--- app/components/PitchingStats.jsx
+++ app/components/PitchingStats.jsx
@@ -37,6 +37,50 @@ export class PitchingStats extends  React.Component {
                         sortType = "numeric"
                         values={pitching}
                     />
+                    <ColumnStats
+                        columns="3"
+                        title="Team WHIP"
+                        heading1="Team" heading2="WHIP" heading3="Points"
+                        key1="team" key2="team_whip" key3="points_whip"
+                        sortKey="points_whip"
+                        sortType = "numeric"
+                        values={pitching}
+                    />
+                    <ColumnStats
+                        columns="3"
+                        title="Team Ks9"
+                        heading1="Team" heading2="Ks9" heading3="Points"
+                        key1="team" key2="team_ks_per_9" key3="points_ks_per_9"
+                        sortKey="points_ks_per_9"
+                        sortType = "numeric"
+                        values={pitching}
+                    />
+                    <ColumnStats
+                        columns="3"
+                        title="Team Wins"
+                        heading1="Team" heading2="Wins" heading3="Points"
+                        key1="team" key2="team_wins" key3="points_wins"
+                        sortKey="points_wins"
+                        sortType = "numeric"
+                        values={pitching}
+                    />
+                    <ColumnStats
+                        columns="3"
+                        title="Team Holds/Saves"
+                        heading1="Team" heading2="Hold/Saves" heading3="Points"
+                        key1="team" key2="team_hold_save" key3="points_hold_save"
+                        sortKey="points_hold_save"
+                        sortType = "numeric"
+                        values={pitching}
+                    />
+                    <ColumnStats
+                        columns="6"
+                        title="Team Summary"
+                        heading1="Team" heading2="ERA" heading3="WHIP" heading4="Ks9" heading5="Wins" heading6="Hold/Saves"
+                        key1="team" key2="team_era" key3="team_whip" key4="team_ks_per_9" key5="team_wins" key6="team_hold_save"
+                        sortKey="team"
+                        values={pitching}
+                    />
                 </div>
             </div>
         );
